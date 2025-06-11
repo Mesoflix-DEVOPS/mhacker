0e909c64ae93f5613ea741d56bc6a1dcb944c543
mesoflix <209336738+mesoflix@users.noreply.github.com>
2025-06-11 11:04:06 +0600

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -285,7 +285,7 @@ const AppWrapper = observer(() => {
                                 'dashboard__chart-wrapper--expanded': is_drawer_open && isDesktop,
                                 'dashboard__chart-wrapper--modal': is_chart_modal_visible && isDesktop,
                             })}>
-                              <iframe src='http://tracktool.netlify.app' height='600px' frameBorder='0' />
+                              <iframe src='https://tracktool.netlify.app' height='600px' frameBorder='0' />
                             </div>
                         </div>
                         <div label={<><FreeBotsIcon /><Localize i18n_default_text='Free Bots' /></>} id='id-free-bots'>
