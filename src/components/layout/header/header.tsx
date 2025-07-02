d84e75a85f5d6e5cec2d0d7ae26ceb0f1c9e4c78
mesoflix <209336738+mesoflix@users.noreply.github.com>
2025-07-03 00:55:17 +0300

    Update header.tsx

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -21,6 +21,7 @@ import './header.scss';
 import React, { useState } from 'react';
 import Modal from '@/components/shared_ui/modal'; // Import the modal component
 
+// Updated InfoIcon with a messages SVG icon
 const InfoIcon = () => {
     const [showModal, setShowModal] = useState(false);
 
@@ -30,7 +31,7 @@ const InfoIcon = () => {
             url: 'https://t.me/lemic2324',
             icon: (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 0C5.37 0 0 5.37 0 12C0 18.63 5.37 24 12 24C18.63 24 24 18.63 24 12C24 5.37 18.63 0 12 0ZM17.94 8.19L15.98 17.03C15.82 17.67 15.42 17.83 14.88 17.52L11.88 15.33L10.44 16.71C10.27 16.88 10.12 17.03 9.79 17.03L10.02 13.97L15.61 8.9C15.87 8.67 15.56 8.54 15.22 8.77L8.21 13.31L5.24 12.38C4.62 12.19 4.61 11.74 5.38 11.43L17.08 7.08C17.6 6.9 18.06 7.23 17.94 8.19Z" fill="#229ED9"/>
+                    <path d="M12 0C5.37 0 0 5.37 0 12C0 18.63 5.37 24 12 24C18.63 24 24 18.63 24 12C24 5.37 18.63 0 12 0ZM17.94 8.19L15.98 17.03C15.82 17.67 15.42 17.83 14.88 17.52L11.88 15.33L10.34 16.81C10.16 16.99 9.98 17.17 9.69 17.17L9.92 14.09L15.58 9.03C15.83 8.81 15.53 8.69 15.19 8.91L8.07 13.54L5.08 12.61C4.45 12.41 4.44 11.96 5.23 11.67L17.02 7.19C17.61 6.98 18.14 7.35 17.94 8.19Z" fill="#229ED9"/>
                 </svg>
             )
         },
@@ -39,7 +40,7 @@ const InfoIcon = () => {
             url: 'mailto:mesoflix254@gmail.com',
             icon: (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM19.6 8.25L12.53 12.67C12.21 12.87 11.79 12.87 11.47 12.67L4.4 8.25C4.15 8.09 4 7.82 4 7.53C4 6.86 4.73 6.46 5.3 6.81L12 11L18.7 6.81C19.27 6.46 20 6.86 20 7.53C20 7.82 19.85 8.09 19.6 8.25Z" fill="#EA4335"/>
+                    <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM19.6 8.25L12.53 12.67C12.21 12.87 11.79 12.87 11.47 12.67L4.4 8.25C4.15 8.09 4.06 7.76 4.22 7.51C4.38 7.26 4.71 7.17 4.96 7.33L12 11.58L19.04 7.33C19.29 7.17 19.62 7.26 19.78 7.51C19.94 7.76 19.85 8.09 19.6 8.25Z" fill="#EA4335"/>
                 </svg>
             )
         },
@@ -48,8 +49,7 @@ const InfoIcon = () => {
             url: 'https://piprhino.site',
             icon: (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM11 14.45L16.95 8.5L15.53 7.08L11 11.61L8.71 9.32L7.29 10.74L11 14.45Z" fill="#4285F4"/>
-                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="#34A853" fillOpacity="0.2"/>
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="#333399"/>
                 </svg>
             )
         },
@@ -58,7 +58,7 @@ const InfoIcon = () => {
             url: 'https://tiktok.com/',
             icon: (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M16.6 5.82C15.9165 5.03962 15.5397 4.03743 15.54 3H12.45V15.4C12.4261 16.071 12.1428 16.7066 11.6597 17.1729C11.1766 17.6393 10.5316 17.8999 9.86 17.91C8.44 17.91 7.26 16.77 7.26 15.36C7.26 13.73 8.76 12.44 10.39 12.76V9.64C7.05 9.34 4.2 11.88 4.2 15.36C4.2 18.71 7 21.02 9.85 21.02C12.89 21.02 15.54 18.37 15.54 15.33V9.01C16.793 9.90985 18.2974 10.3926 19.84 10.39V7.3C19.84 7.3 17.96 7.39 16.6 5.82Z" fill="black"/>
+                    <path d="M16.6 5.82C15.9165 5.03962 15.5397 4.03743 15.54 3H12.45V15.4C12.4261 16.071 12.1428 16.7066 11.6597 17.1729C11.1766 17.6393 10.5316 17.8999 9.86 17.91C8.44 17.91 7.26 16.74 7.26 15.33C7.26 13.92 8.44 12.75 9.86 12.75C10.13 12.75 10.39 12.78 10.64 12.83V10.22C10.37 10.18 10.1 10.16 9.83 10.16C6.96 10.16 4.61 12.52 4.61 15.39C4.61 18.26 6.96 20.62 9.83 20.62C12.7 20.62 15.05 18.26 15.05 15.39V8.19C16.05 8.71 17.19 9 18.41 9V5.82H16.6Z" fill="#000"/>
                 </svg>
             )
         },
@@ -67,7 +67,7 @@ const InfoIcon = () => {
             url: 'https://wa.me/254723913800',
             icon: (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 2C6.48 2 2 6.48 2 12C2 13.85 2.49 15.55 3.36 17.02L2.05 21.95L7.08 20.66C8.51 21.48 10.19 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM16.53 15.5C16.37 15.93 15.71 16.33 15.19 16.43C14.5 16.57 13.96 16.48 12.06 15.75C9.54 14.78 7.9 12.23 7.77 12.07C7.64 11.91 6.76 10.73 6.76 9.5C6.76 8.27 7.4 7.66 7.65 7.39C7.9 7.12 8.18 7.05 8.36 7.05C8.54 7.05 8.72 7.05 8.88 7.06C9.04 7.07 9.27 7 9.49 7.47C9.71 7.94 10.18 9.17 10.25 9.31C10.32 9.45 10.36 9.62 10.27 9.82C9.75 10.93 9.17 10.86 9.54 11.47C10.41 12.87 11.38 13.47 12.62 14.09C12.89 14.23 13.06 14.21 13.21 14.04C13.36 13.87 13.81 13.35 13.98 13.11C14.15 12.87 14.32 12.91 14.54 12.99C14.76 13.07 15.98 13.67 16.23 13.8C16.48 13.93 16.64 13.99 16.71 14.09C16.78 14.19 16.78 14.57 16.53 15.5Z" fill="#25D366"/>
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 13.85 2.49 15.55 3.36 17.02L2.05 21.95L7.08 20.66C8.51 21.48 10.19 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM16.53 15.5C16.37 15.78 15.86 16.01 15.39 16.1C15.18 16.14 14.85 16.22 14.24 16.07C13.33 15.85 11.94 15.17 10.77 14.01C9.6 12.85 8.93 11.46 8.71 10.55C8.56 9.94 8.64 9.61 8.68 9.4C8.77 8.93 9 8.42 9.28 8.27C9.44 8.19 9.65 8.13 9.8 8.16C10.01 8.2 10.23 8.45 10.37 8.67C10.47 8.83 10.57 9.07 10.66 9.25C10.77 9.47 10.71 9.62 10.58 9.8C10.53 9.87 10.47 9.98 10.52 10.06C11.12 11.01 11.98 11.87 12.93 12.47C13.01 12.52 13.12 12.46 13.19 12.41C13.37 12.28 13.52 12.22 13.74 12.33C13.92 12.43 14.16 12.53 14.32 12.63C14.54 12.77 14.79 12.99 14.83 13.2C14.86 13.36 14.8 13.57 14.72 13.73Z" fill="#25D366"/>
                 </svg>
             )
         }
@@ -79,9 +79,10 @@ const InfoIcon = () => {
                 className="info-icon"
                 onClick={() => setShowModal(true)}
             >
-            <svg width="32" height="32" viewBox="0 0 24 24" fill="none">
-  <path d="M21 6.5a2.5 2.5 0 0 0-2.5-2.5h-13A2.5 2.5 0 0 0 3 6.5v11A2.5 2.5 0 0 0 5.5 20H19l2 2V6.5zM5.5 5h13A1.5 1.5 0 0 1 20 6.5v11a1.5 1.5 0 0 1-1.5 1.5H5.5A1.5 1.5 0 0 1 4 17.5v-11A1.5 1.5 0 0 1 5.5 5z" fill="#555"/>
-</svg>
+                {/* Messages Icon SVG */}
+                <svg width="32" height="32" viewBox="0 0 24 24" fill="none">
+                    <path d="M21 6.5a2.5 2.5 0 0 0-2.5-2.5h-13A2.5 2.5 0 0 0 3 6.5v11A2.5 2.5 0 0 0 5.5 20H19l2 2V6.5zM5.5 5h13A1.5 1.5 0 0 1 20 6.5v11a1.5 1.5 0 0 1-1.5 1.5H5.5A1.5 1.5 0 0 1 4 17.5v-11A1.5 1.5 0 0 1 5.5 5z" fill="#555"/>
+                </svg>
             </button>
 
             <Modal
@@ -121,27 +122,7 @@ const AppHeader = observer(() => {
 
     const { isOAuth2Enabled } = useOauth2();
 
-    const [isToggled, setIsToggled] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [stake, setStake] = useState('');
-    const [martingale, setMartingale] = useState('');
-
-    const handleToggle = () => {
-        if (!isToggled) {
-            setIsModalOpen(true); // Open modal when toggled on
-        } else {
-            setIsToggled(false); // Turn off toggle
-        }
-    };
-
-    const handleProceed = () => {
-        if (stake.trim() && martingale.trim()) {
-            setIsToggled(true); // Enable toggle only if inputs are valid
-            setIsModalOpen(false); // Close modal
-        } else {
-            alert('Please enter valid Stake and Martingale values.');
-        }
-    };
+    // Removed the states for martingale/stake modal and toggle
 
     const renderAccountSection = () => {
         if (isAuthorizing) {
@@ -218,47 +199,9 @@ const AppHeader = observer(() => {
                 <AppLogo />
                 <MobileMenu />
                 <InfoIcon />
-                <button
-                    className="app-header__toggle"
-                    onClick={handleToggle}
-                    aria-pressed={isToggled}
-                >
-                    {isToggled ? 'ON' : 'OFF'}
-                </button>
+                {/* Removed martingale/stake toggle and modal */}
             </Wrapper>
             <Wrapper variant='right'>{renderAccountSection()}</Wrapper>
-
-            {isModalOpen && (
-                <Modal
-                    is_open={isModalOpen}
-                    toggleModal={() => setIsModalOpen(false)}
-                    title="Select Stake and Martingale"
-                >
-                    <div className="modal-content">
-                        <label>
-                            Stake:
-                            <input
-                                type="number"
-                                value={stake}
-                                onChange={e => setStake(e.target.value)}
-                                placeholder="Enter stake"
-                            />
-                        </label>
-                        <label>
-                            Martingale:
-                            <input
-                                type="number"
-                                value={martingale}
-                                onChange={e => setMartingale(e.target.value)}
-                                placeholder="Enter martingale"
-                            />
-                        </label>
-                        <button onClick={handleProceed} className="proceed-button">
-                            Proceed
-                        </button>
-                    </div>
-                </Modal>
-            )}
         </Header>
     );
 });
