a22f29620b50b061ad7a0b81eae94565a1b2ec9d
Osam533 <osamwriting137@gmail.com>
2025-09-03 16:59:44 +0300

    Update use-mobile-menu-config.tsx

--- src/components/layout/header/mobile-menu/use-mobile-menu-config.tsx
+++ src/components/layout/header/mobile-menu/use-mobile-menu-config.tsx
@@ -1,32 +1,13 @@
 import { ComponentProps, ReactNode } from 'react';
-import Livechat from '@/components/chat/Livechat';
-import useIsLiveChatWidgetAvailable from '@/components/chat/useIsLiveChatWidgetAvailable';
-import { standalone_routes } from '@/components/shared';
-import { useOauth2 } from '@/hooks/auth/useOauth2';
-import useRemoteConfig from '@/hooks/growthbook/useRemoteConfig';
-import { useIsIntercomAvailable } from '@/hooks/useIntercom';
 import useThemeSwitcher from '@/hooks/useThemeSwitcher';
 import RootStore from '@/stores/root-store';
-import {
-    LegacyAccountLimitsIcon,
-    LegacyCashierIcon,
-    LegacyChartsIcon,
-    LegacyHelpCentreIcon,
-    LegacyHomeOldIcon,
-    LegacyLogout1pxIcon,
-    LegacyProfileSmIcon,
-    LegacyResponsibleTradingIcon,
-    LegacyTheme1pxIcon,
-    LegacyWhatsappIcon,
-} from '@deriv/quill-icons/Legacy';
-import { BrandDerivLogoCoralIcon } from '@deriv/quill-icons/Logo';
-import { useTranslations } from '@deriv-com/translations';
+import { LegacyLogout1pxIcon, LegacyTheme1pxIcon } from '@deriv/quill-icons/Legacy';
 import { ToggleSwitch } from '@deriv-com/ui';
-import { URLConstants } from '@deriv-com/utils';
+import { useTranslations } from '@deriv-com/translations';
+import { useOauth2 } from '@/hooks/auth/useOauth2';
 
 export type TSubmenuSection = 'accountSettings' | 'cashier';
 
-//IconTypes
 type TMenuConfig = {
     LeftComponent: ReactNode | React.ElementType;
     RightComponent?: ReactNode;
@@ -42,23 +23,10 @@ type TMenuConfig = {
 const useMobileMenuConfig = (client?: RootStore['client']) => {
     const { localize } = useTranslations();
     const { is_dark_mode_on, toggleTheme } = useThemeSwitcher();
-
     const { oAuthLogout } = useOauth2({ handleLogout: async () => client?.logout(), client });
-
-    const { data } = useRemoteConfig(true);
-    const { cs_chat_whatsapp } = data;
-
-    const { is_livechat_available } = useIsLiveChatWidgetAvailable();
-    const icAvailable = useIsIntercomAvailable();
 
     const menuConfig: TMenuConfig[] = [
         [
-            {
-                as: 'a',
-                href: 'https://osamtradinghub.com',
-                label: localize('osamhnr'),
-                LeftComponent: BrandDerivLogoCoralIcon,
-            },
             {
                 as: 'button',
                 label: localize('Dark theme'),
@@ -66,29 +34,6 @@ const useMobileMenuConfig = (client?: RootStore['client']) => {
                 RightComponent: <ToggleSwitch value={is_dark_mode_on} onChange={toggleTheme} />,
             },
         ],
-        (
-            [
-                cs_chat_whatsapp
-                    ? {
-                          as: 'a',
-                          href: 'https://wa.link/vr3sao',
-                          label: localize('WhatsApp'),
-                          LeftComponent: LegacyWhatsappIcon,
-                          target: '_blank',
-                      }
-                    : null,
-                is_livechat_available || icAvailable
-                    ? {
-                          as: 'button',
-                          label: localize('Live chat'),
-                          LeftComponent: Livechat,
-                          onClick: () => {
-                              icAvailable ? window.Intercom('show') : window.LiveChatWidget?.call('maximize');
-                          },
-                      }
-                    : null,
-            ] as TMenuConfig
-        ).filter(Boolean),
         client?.is_logged_in
             ? [
                   {
