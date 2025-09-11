84a3dfead7e85d6df5395dbc23d59ee286d1f03f
Osam533 <osamwriting137@gmail.com>
2025-09-11 09:25:59 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -358,12 +358,12 @@ const AppWrapper = observer(() => {
             >
               <div className="free-bots">
                 {/* Social Media Icons */}
-                 <div className="social-media-container">
-                  <a href="https://youtube.com/@osamtradinghub-cl1fs?si=JSF3lDV1TBzjUTTb" target="_blank" rel="noopener noreferrer" className="social-icon youtube-icon"><YouTubeIcon /></a>
-                  <a href="https://www.instagram.com/osamtradinghub.com1?igsh=Mmh2aW43a3dpamRq" target="_blank" rel="noopener noreferrer" className="social-icon instagram-icon"><InstagramIcon /></a>
-                  <a href="https://chat.whatsapp.com/E2cZOyZr75VExcbkprwuTe?mode=ac_t" target="_blank" rel="noopener noreferrer" className="social-icon whatsapp-icon"><WhatsAppIcon /></a>
-                  <a href="https://www.tiktok.com/@_its_osam?_t=ZM-8yu0PcOKRHR&_r=1" target="_blank" rel="noopener noreferrer" className="social-icon tiktok-icon"><TikTokIcon /></a>
-                  <a href="https://t.me/+dLoQvTnT_2wzOGY0" target="_blank" rel="noopener noreferrer" className="social-icon telegram-icon"><TelegramIcon /></a>
+                <div className="social-media-container">
+                  <a href="https://youtube.com/@danielmwangi3263?si=mXljcLwrAfDJmP2L" target="_blank" rel="noopener noreferrer" className="social-icon youtube-icon"><YouTubeIcon /></a>
+                  <a href="https://www.instagram.com/daniel_the_ceo_01?igsh=cmRnZHh6MHRibThy" target="_blank" rel="noopener noreferrer" className="social-icon instagram-icon"><InstagramIcon /></a>
+                  <a href="https://whatsapp.com/channel/0029Vb71CLFLtOj6rhqG1A3E" target="_blank" rel="noopener noreferrer" className="social-icon whatsapp-icon"><WhatsAppIcon /></a>
+                  <a href="https://www.tiktok.com/@danieltheceo01?_t=ZM-8z0BoHRxMve&_r=1" target="_blank" rel="noopener noreferrer" className="social-icon tiktok-icon"><TikTokIcon /></a>
+                  <a href="https://t.me/derihub" target="_blank" rel="noopener noreferrer" className="social-icon telegram-icon"><TelegramIcon /></a>
                 </div>
                 <div className="free-bots__content-wrapper">
                   <div className="free-bots__content">
