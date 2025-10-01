ad0cf4a7abe3acff003ffb69e290d43cd169ff65
Mesoflix-DEVOPS <projectmesoflix@gmail.com>
2025-10-01 22:27:00 +0300

    Update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
-export const website_domain = 'osamhnr.com';
-export const website_name = 'osamhnr.com';
+export const website_domain = 'FREEDOMHACKER';
+export const website_name = 'freedomhacke.site';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
