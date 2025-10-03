27f08257b532a6ea74452c0ebbfbf0d03e4b2f37
Mesoflix-DEVOPS <projectmesoflix@gmail.com>
2025-10-03 18:57:43 +0300

    Create main.tsx

---
+++ src/pages/advanced/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ApolloAnalysisPage from './apollo_analysis/analysis';
+import './style.css';
+
+const AnalysisPage = () => {
+    return (
+        <div className='main_analysis'>
+            <ApolloAnalysisPage />
+        </div>
+    );
+};
+
+export default AnalysisPage;
