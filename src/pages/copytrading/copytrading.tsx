d35a9ed6cd2f5cf8cff3f297a0b8a5e36c819aa6
Mesoflix-DEVOPS <projectmesoflix@gmail.com>
2025-10-24 19:17:41 +0300

    Update copytrading.tsx

--- src/pages/copytrading/copytrading.tsx
+++ src/pages/copytrading/copytrading.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useRef, useEffect } from "react"
-import styles from "./copy-trading-page.module.scss"
+import styles from "./CopyTradingPage.module.scss"
 
 interface Account {
   loginid: string
